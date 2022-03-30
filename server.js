0fe48a105cc9289376c223866d66b86553967419
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-03-30 07:23:21 +0100

    first commit

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
 const app = express();
+app.use(express.json());
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Welcome to my TeamLance Project...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `${process.env.APP_NAME} is running on PORT: ${PORT}, in MODE: ${process.env.APP_MODE}`
+  )
+);
+export default app;
